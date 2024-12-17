33f77bfb81284d04facc3d8fa1febe466bd96524
Oleh Makarytskyi <makarytskyi.o@ajax.systems>
2024-12-17 11:24:56 +0200

    begin working on cases

--- src/main/kotlin/systems/ajax/codetests/infrastructure/testrail/DifferencePublisher.kt
+++ src/main/kotlin/systems/ajax/codetests/infrastructure/testrail/DifferencePublisher.kt
@@ -2,6 +2,7 @@ package systems.ajax.codetests.infrastructure.testrail
 
 import org.springframework.stereotype.Component
 import systems.ajax.codetests.application.model.diff.Type
+import systems.ajax.codetests.application.port.input.ApplyDiffCaseInPort
 import systems.ajax.codetests.application.port.input.ApplyDiffSectionInPort
 import systems.ajax.codetests.application.port.input.FileDifferenceInPort
 import systems.ajax.codetests.application.port.output.TestrailManagerOutPort
@@ -10,13 +11,17 @@ import systems.ajax.codetests.application.port.output.TestrailManagerOutPort
 internal class DifferencePublisher(
     private val fileDifferenceInPort: FileDifferenceInPort,
     private val applyDiffSectionInPort: ApplyDiffSectionInPort,
+    private val applyDiffCaseInPort: ApplyDiffCaseInPort,
 ) : TestrailManagerOutPort {
 
     override fun publish() {
         fileDifferenceInPort.getDifference()
             .forEach { (typeAction, files) ->
                 when (typeAction.type) {
-                    Type.CASE -> TODO()
+                    Type.CASE -> files.forEach {
+                        applyDiffCaseInPort.execute(it, typeAction.action)
+                    }
+
                     Type.SECTION -> files.forEach {
                         applyDiffSectionInPort.execute(it, typeAction.action)
                     }
