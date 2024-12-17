abce9dc3622ce11ccbf5227b209950175bd5e644
Oleh Makarytskyi <makarytskyi.o@ajax.systems>
2024-12-17 14:43:49 +0200

    changed

--- src/main/kotlin/systems/ajax/codetests/application/port/output/CaseManagerOutPort.kt
+++ src/main/kotlin/systems/ajax/codetests/application/port/output/CaseManagerOutPort.kt
@@ -5,10 +5,5 @@ import systems.ajax.codetests.application.model.FilePath
 import systems.ajax.codetests.application.model.diff.TypeAction
 
 interface CaseManagerOutPort {
-//    fun add(appSection: AppSection): Int
-//    fun update(appSection: AppSection)
-//    fun delete(filePath: FilePath)
-//    fun move(appSection: AppSection)
-
     fun publishDiff(differenceMap: Map<TypeAction, List<FilePath>>): List<FileId>
 }
